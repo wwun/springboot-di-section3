509e7870a10bf5ff59c4c3b032867eb15a2151f5
William <williamwun@gmail.com>
2023-12-28 17:45:43 -0800

    Video 45 finished

---
+++ src/main/java/com/william/springboot/di/app/springbootdi/models/Product.java
@@ -0,0 +1,42 @@
+package com.william.springboot.di.app.springbootdi.models;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private Long price;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, Long price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+}
---
+++ src/main/java/com/william/springboot/di/app/springbootdi/repositories/ProductRepository.java
@@ -0,0 +1,24 @@
+package com.william.springboot.di.app.springbootdi.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.william.springboot.di.app.springbootdi.models.Product;
+
+public class ProductRepository {
+    private List<Product> data;
+
+    public ProductRepository() {
+        this.data = Arrays.asList(new Product(1L, "Memoria corsair", 300L), new Product(2L, "Cpu Intel Core", 85L),
+                new Product(3L, "Teclado Razer", 180L), new Product(4L, "Motherboard Gigabyte", 490L));
+    }
+
+    public List<Product> findAll() {
+        return data;
+    }
+
+    public Product findById(Long id) {
+        return data.stream().filter(p -> p.getId().equals(id)).findFirst().orElse(null);
+    }
+
+}
